fde083ae818c7ba060650f25fd5bd63b4b187686
Berraiz <jfsnow00@gmail.com>
2025-01-30 19:29:02 -0800

    v2

--- form.php
+++ form.php
@@ -1,11 +1,12 @@
-<html>
 <form action="process.php" method="post">
-<label>Value for a: </label>
-<input type="number" name="a" required>
-<label>Value for b: </label>
-<input type="number" name="b" required>
-<label>Value for c: </label>
-<input type="number" name="c" required>
-<input type="submit" value="calculate">
+    <label for="a">Enter a:</label>
+    <input type="number" name="a" step="any" required><br><br>
+
+    <label for="b">Enter b:</label>
+    <input type="number" name="b" step="any" required><br><br>
+
+    <label for="c">Enter c:</label>
+    <input type="number" name="c" step="any" required><br><br>
+
+    <input type="submit" name="submit" value="Calculate">
 </form>
-</html>
